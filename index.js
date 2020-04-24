2a40148240d66b0621d289f6c37e810b0321f8fb
hoanggaphan <phansihoang1998@gmail.com>
2020-04-24 20:49:24 +0700

    query parameter

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
+const users = [
+  { id: 1, name: "Kula" },
+  { id: 2, name: "K'" },
+];
 
 app.set("view engine", "pug");
 app.set("views", "./views");
 
 app.get("/", (req, res) => res.render("index", { name: "Nameless" }));
-app.get("/users", (req, res) =>
-  res.render("users/index", {
-    usersList: [
-      { id: 1, name: "Kula" },
-      { id: 2, name: "K'" },
-    ],
-  })
-);
+app.get("/users", (req, res) => res.render("users/index", { users }));
+
+app.get("/users/search", (req, res) => {
+  const q = req.query.q;
+  const matchedUses = users.filter(
+    (user) => user.name.toLocaleLowerCase().indexOf(q.toLocaleLowerCase()) > -1
+  );
+  res.render("users/index", { users: matchedUses, q });
+});
 
 app.listen(3000, () => console.log("App listening on port " + port));
