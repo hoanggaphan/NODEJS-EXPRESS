4bcf6299d137f7623dd9211af505e1d2ea3a9606
hoanggaphan <phansihoang1998@gmail.com>
2020-04-24 19:06:02 +0700

    install, initilization express, pug

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/", (req, res) => res.render("index", { name: "Nameless" }));
+app.get("/users", (req, res) =>
+  res.render("users/index", {
+    usersList: [
+      { id: 1, name: "Kula" },
+      { id: 2, name: "K'" },
+    ],
+  })
+);
+
 app.listen(3000, () => console.log("App listening on port " + port));
