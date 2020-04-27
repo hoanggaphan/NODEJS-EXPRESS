385076d2bd0651791721fa95ec34d7453b483c54
hoanggaphan <phansihoang1998@gmail.com>
2020-04-27 18:59:29 +0700

    Signed cookie

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("views", "./views");
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
+app.use(cookieParser("adASdas213aASD213sdas"));
 
 app.use(express.static("public"));
 
