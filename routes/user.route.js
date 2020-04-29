daea947c8a8e963a6584819569d6bb7b634509cf
hoanggaphan <phansihoang1998@gmail.com>
2020-04-29 18:57:25 +0700

    upload image

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
+const upload = require("../middlewares/upload.middleware");
 const controllers = require("../controllers/user.controller");
-const validate = require('../validates/user.validate');
+const validate = require("../validates/user.validate");
 
 const router = express.Router();
 
 router.get("/", controllers.index);
 
-router.get('/cookie', (req, res, next) => {
-  res.cookie('session-id', 123);
+router.get("/cookie", (req, res, next) => {
+  res.cookie("session-id", 123);
   res.send("Demo Cookie");
-})
+});
 
 router.get("/search", controllers.search);
 
@@ -18,6 +19,6 @@ router.get("/create", controllers.create);
 
 router.get("/:id", controllers.get);
 
-router.post("/create", validate.postCreate, controllers.postCreate);
+router.post("/create", upload, validate.postCreate, controllers.postCreate);
 
 module.exports = router;
