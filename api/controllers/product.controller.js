0140e1fa59a3c0daf80d115f00a9cc1c84ab6bb5
hoanggaphan <phansihoang1998@gmail.com>
2020-05-11 20:41:49 +0700

    rest API

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,8 +1,31 @@
 const Product = require("./../../models/product.model");
 
 module.exports.index = async (req, res) => {
-  res.locals.title = "Products";
-
   const products = await Product.find();
   res.json(products);
+};
+
+module.exports.view = async (req, res) => {
+  const productId = req.params.id;
+  const product = await Product.findById(productId);
+  res.json(product);
+};
+
+module.exports.create = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.json(product);
+};
+
+module.exports.update = async (req, res) => {
+  const productId = req.params.id;
+  const product = await Product.findByIdAndUpdate(productId, req.body, {
+    new: true,
+  });
+  res.json(product);
+};
+
+module.exports.delete = async (req, res) => {
+  const productId = req.params.id;
+  const product = await Product.findByIdAndDelete(productId);
+  res.json(product);
 };
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -5,4 +5,14 @@ const controllers = require("../controllers/product.controller");
 
 router.get("/", controllers.index);
 
+router.get("/:id", controllers.view);
+
+// router.put("/:id", controllers.update);
+
+router.patch("/:id", controllers.update);
+
+router.post("/", controllers.create);
+
+router.delete("/:id", controllers.delete)
+
 module.exports = router;
